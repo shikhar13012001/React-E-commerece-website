8d3dfc7ce26ebee8145749a8d2722b049b853d41
unknown <Shikhar>
2021-07-13 20:41:29 +0530

    added files for homepage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+import HomePage from './components/homepage.components'
+function App() {
+  return (
+    <div>
+  <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
