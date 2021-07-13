e02771beeff0550815913257b9aff394c13dfb28
unknown <Shikhar>
2021-07-13 23:36:54 +0530

    components built and css is compleleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import HomePage from './components/homepage.components'
+import HomePage from './Pages/homepage/homepage.components'
 function App() {
   return (
     <div>
---
+++ src/Pages/homepage/homepage.components.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../components/directory-menu/directory-menu.components'
+const HomePage= () =>{
+    return ( <Directory/>
+    )
+
+}
+export default HomePage;
