ae0f86c04fbfc22cc7924d0ac9fbe3ab9c5580eb
unknown <Shikhar>
2021-07-14 17:21:28 +0530

    header and images have been made

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import HomePage from './Pages/homepage/homepage.components'
 import {Route,Switch} from 'react-router-dom'
 import ShopPage from './Pages/shop/shop.component';
- 
+ import Header from"./components/Header/header.component"
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
