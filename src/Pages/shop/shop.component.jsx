e2856c57a70c3de1eda2aec3d14bd09e39c6433b
unknown <Shikhar>
2021-08-01 14:55:32 +0530

    backend updated

--- src/Pages/shop/shop.component.jsx
+++ src/Pages/shop/shop.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import {Route} from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
-const ShopPage = ({collections }) =>{
+import CategoryPage from '../collection/collection.component';
+const ShopPage = ({match }) =>{
     
  
         return(
         <div className="shop-page">
-            
-        <CollectionsOverview/>
+      
+        <Route exact path={`${match.path}`} component= {CollectionsOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CategoryPage}/>
         </div>
         )
     }
