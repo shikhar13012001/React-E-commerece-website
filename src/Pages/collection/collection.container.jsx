5aca7f8ca95ebafbc3ab493bc49ae22ddd2e42e1
shikhar13012001 <75368010+shikhar13012001@users.noreply.github.com>
2021-08-13 13:23:46 +0530

    fixed bug

--- src/Pages/collection/collection.container.jsx
+++ src/Pages/collection/collection.container.jsx
@@ -5,7 +5,7 @@ import { selectCollectionIsLoaded } from "../../redux/shop/shop.selector";
 import withSpinner from "../../components/with-spinned/with-spinner.component";
 import CategoryPage from "./collection.component";
 const mapStateToProps = createStructuredSelector({
-  isLoading: (state) => !selectCollectionIsLoaded(state),
+  isLoading: state => !selectCollectionIsLoaded(state),
 });
 const CollectionPageContainer= compose(
     connect((mapStateToProps),withSpinner)(CategoryPage)
